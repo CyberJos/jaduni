b8fd27bdf87c015973c3eea204ec334d2e4d6366
Joseph Kuo <cyberjos@gmail.com>
2014-05-03 05:52:36 +0800

    Added a render engine holder and several static methods for management.

--- src/main/java/org/cyberjos/jaduni/RenderEngine.java
+++ src/main/java/org/cyberjos/jaduni/RenderEngine.java
@@ -22,6 +22,11 @@ package org.cyberjos.jaduni;
 
 import java.io.Reader;
 import java.io.Writer;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.lang3.Validate;
 
 /**
  * {@code RenderEngine} is an interface which is responsible for converting the
@@ -32,11 +37,107 @@ import java.io.Writer;
  * render engine parses the given input, converts it by following predefined
  * rules of conversion, and then output to the specified destination. The output
  * can be a string, or a {@code Writer} object for output.
+ * <p />
+ * This interface contains a singleton enumeration and uses it to simply manage
+ * all render engine. However, a developer only needs to deal with this
+ * interface without accessing the enumeration directly:
+ *
+ * <pre>
+ * // Retrieve the default render engine which is designed for Creole
+ * RenderEngine defaultRenderEngine = RenderEngine.getDefault();
+ * 
+ * // Add or retrieve a new render engine
+ * RenderEngine.set(&quot;MyRenderEngine&quot;, new MyRenderEngine());
+ * RenderEngine myRenderEngine = RenderEngine.get(&quot;MyRenderEngine&quot;);
+ * 
+ * // Remove a render engine. It cannot remove the default render engine.
+ * RenderEngine.set(&quot;MyRenderEngine&quot;, null);
+ * </pre>
  *
  * @author Joseph S. Kuo
  * @since 0.0.1, 2014/4/30
  */
 public interface RenderEngine {
+    /**
+     * The {@code RenderEngine} holder is a singleton enumeration for easily
+     * managing all render engines in a JVM. It creates and registers the
+     * default render engine by default while initialization. A user doesn't
+     * need to use this enumeration to manage render engines. Instead, he should
+     * use those static methods in {@link RenderEngine} to get or set a render
+     * engine object associated with the specified name.
+     *
+     * @author Joseph S. Kuo
+     * @since 0.0.1, 2014/5/2
+     */
+    public enum Holder {
+        /**
+         * Singleton instance.
+         */
+        INSTANCE;
+
+        /**
+         * Constant for default render engine.
+         */
+        private static final String DEFAULT_RENDER_ENGINE = "DefaultRenderEngine-RenderEngineImpl.class";
+
+        /**
+         * The map to store render engines.
+         */
+        final Map<String, RenderEngine> renderEngineMapping = new ConcurrentHashMap<>();
+
+        /**
+         * Constructs without any arguments. It creates the default render
+         * engine and put it into mapping while initialization.
+         */
+        private Holder() {
+            this.renderEngineMapping.put(DEFAULT_RENDER_ENGINE, new RenderEngineImpl());
+        }
+    }
+
+    /**
+     * Returns the default render engine. It never returns a {@code null}
+     * object.
+     *
+     * @return the default render engine which is never {@code null}
+     */
+    public static RenderEngine getDefault() {
+        return Holder.INSTANCE.renderEngineMapping.get(Holder.DEFAULT_RENDER_ENGINE);
+    }
+
+    /**
+     * Returns the render engine associated with the given name, or {@code null}
+     * if not found.
+     *
+     * @param renderEngineName the name of the render engine
+     * @return the render engine associated with the given name, or {@code null}
+     *         if not found
+     * @throws NullPointerException if the given name is {@code null}
+     * @throws IllegalArgumentException if the given name is blank
+     */
+    public static RenderEngine get(final String renderEngineName) {
+        Validate.notBlank(renderEngineName, "The given name must not be null.");
+        return Holder.INSTANCE.renderEngineMapping.get(renderEngineName);
+    }
+
+    /**
+     * Sets the render engine into the mapping with the given name. The passed
+     * render engine can be {@code null}.
+     *
+     * @param renderEngineName the name of render engine
+     * @param renderEngine the render engine. It can be {@code null}
+     * @throws NullPointerException if the given name is {@code null}
+     * @throws IllegalArgumentException if the given name is blank
+     */
+    public static void set(final String renderEngineName, final RenderEngine renderEngine) {
+        Validate.notBlank(renderEngineName, "The given name must not be null.");
+
+        if (Holder.DEFAULT_RENDER_ENGINE.equals(renderEngineName)) {
+            Objects.requireNonNull(renderEngine, "Cannot set the default render engine to a null object.");
+        }
+
+        Holder.INSTANCE.renderEngineMapping.put(renderEngineName, renderEngine);
+    }
+
     /**
      * Parses the given source and then returns the converted string. The source
      * could be a string, or a location of data source which contains text. It
